73e7a001bb81d8ca87d31ed32382ba3fe44eb3fb
Rachit Mishra <twntee@gmail.com>
2018-10-28 02:17:09 +0530

    Update UiState.kt

--- app/src/main/java/com/practo/meetup/UiState.kt
+++ app/src/main/java/com/practo/meetup/UiState.kt
@@ -14,6 +14,7 @@ enum class UiState {
 
 interface UiStateManager {
 
+    // Interface segregation and providing default implementations inside an inteface
     fun initStateObserver(lifeCycle: LifecycleOwner, uiState: LiveData<UiState>) {
         uiState.observe(lifeCycle, Observer {
             when (it) {
