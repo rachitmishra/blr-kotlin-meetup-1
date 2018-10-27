f160d1f0333d996e2fc4498dbbf14dbae88d543f
Rachit Mishra <twntee@gmail.com>
2018-10-28 02:11:10 +0530

    Update BaseViewManager.kt

--- app/src/main/java/com/practo/meetup/BaseViewManager.kt
+++ app/src/main/java/com/practo/meetup/BaseViewManager.kt
@@ -1,6 +1,10 @@
 package com.practo.meetup
 
+// A basic interface which manages UI states in View/ViewModel
 interface BaseViewManager {
+    
+    // function with default value
+    // rather than providing the param value at each call site we can set default values to function params 
     fun handleError(connectionError: Boolean = false, error: String = "")
     fun handleEmpty(message: String = "")
     fun showProgressView(message: String = "")
